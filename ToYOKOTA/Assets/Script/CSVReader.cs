84445e017770940a2f07e4d8ba8cd477d631ddb4
otoyori <yokota1022@outlook.jp>
2019-08-24 08:59:34 +0900

    first commit

---
+++ ToYOKOTA/Assets/Script/CSVReader.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class CSVReader : MonoBehaviour
+{
+
+    public List<string[]> csvDatas = new List<string[]>();
+
+    public void CsvRead(string Name)
+    {
+        // csvをロード
+        TextAsset csv = Resources.Load(Name) as TextAsset;
+        StringReader reader = new StringReader(csv.text);
+        while (reader.Peek() != -1)
+        {
+            // ','ごとに区切って配列へ格納
+            string line = reader.ReadLine();
+            csvDatas.Add(line.Split(','));
+        }
+    }
+}
---
+++ ToYOKOTA/Assets/Script/GameEvent.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameEvent : MonoBehaviour {
+    //public string seasoningName;
+    private CSVReader seasoningData = new CSVReader();
+    public Text NaviText;
+
+    // Use this for initialization
+    void Start () {
+        
+    }
+	
+	// Update is called once per frame
+	void Update () {
+    }
+    public void OnClick()
+    {
+        seasoningData = new CSVReader();
+        seasoningData.CsvRead("test");
+        NaviText.text = seasoningData.csvDatas[0][0] + "に" + seasoningData.csvDatas[0][1] + "を置いてください";
+        ImageChange();
+    }
+    public void ImageChange()
+    {
+        Texture2D texture = Resources.Load("Images/" + seasoningData.csvDatas[0][1]) as Texture2D;
+        Image img = GameObject.Find("Canvas/Cube(" + seasoningData.csvDatas[0][0] + ")").GetComponent<Image>();
+        img.sprite = Sprite.Create(texture, new Rect(0, 0, texture.width, texture.height), Vector2.zero);
+        
+    }
+}
